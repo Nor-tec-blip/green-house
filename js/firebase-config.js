f9dd82943b0b41736b707a0cfcb77e7c5d101cc7
Nor-tec-blip <wicakuta@gmail.com>
2026-05-18 09:15:03 +0700

    firebace update

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,14 +6,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzBDdHWLMkWN7bH3oJBdri6s5KRBM6EHk",
-  authDomain: "monitoring-iot-29ac6.firebaseapp.com",
-  databaseURL: "https://monitoring-iot-29ac6-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "monitoring-iot-29ac6",
-  storageBucket: "monitoring-iot-29ac6.firebasestorage.app",
-  messagingSenderId: "4724063661",
-  appId: "1:4724063661:web:0a03e34b61072f9fd97524",
-  measurementId: "G-2Y44SR9LMN"
+  apiKey: "AIzaSyBE88gFIwE3gxH52qEgkr6zmVSxH8kZp1Y",
+  authDomain: "greenhousex-38d4c.firebaseapp.com",
+  databaseURL: "https://greenhousex-38d4c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "greenhousex-38d4c",
+  storageBucket: "greenhousex-38d4c.firebasestorage.app",
+  messagingSenderId: "527631702104",
+  appId: "1:527631702104:web:51f8f1e2d4c9e2cd7f922d"
 };
 
 const app = initializeApp(firebaseConfig);
